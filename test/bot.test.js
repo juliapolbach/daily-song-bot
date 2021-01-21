255d1301b03a74868a922c438981a54a4192f894
juliapolbach <juliapolbach@gmail.com>
2021-01-21 17:35:17 +0100

    added a sinon stub

--- test/bot.test.js
+++ test/bot.test.js
@@ -1,12 +1,12 @@
 'use strict'
-const assert = require('assert');
-const sinon = require('sinon');
+const assert = require('assert')
+const sinon = require('sinon')
 const {Twit, tweet} = require('../bin/bot')
 
 describe('Tweet', ()=> {
     it('returns success message', async () => {
         const stub = sinon.stub(Twit, 'post')
-        stub.resolves('Message tweeted')
+        stub.resolves('Message tweeted!')
         const response = await tweet()
         assert.deepStrictEqual(response, 'Message tweeted!')
         stub.restore()
